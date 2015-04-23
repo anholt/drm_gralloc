e3814466abac1b8e65e53e4377bc21390514ebcf
Paulo Sergio Travaglia <pstglia@gmail.com>
2015-04-22 23:15:48 -0300

    Enable radeonsi on Android-x86

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@ DRM_GPU_DRIVERS := $(strip $(filter-out swrast, $(BOARD_GPU_DRIVERS)))
 
 freedreno_drivers := freedreno
 intel_drivers := i915 i965 i915g ilo
-radeon_drivers := r300g r600g
+radeon_drivers := r300g r600g radeonsi
 nouveau_drivers := nouveau
 vmwgfx_drivers := vmwgfx
 
